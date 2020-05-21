8eef09e9cdf61e7425b31e35511adbd0fe45010a
Sharath7 <sharathpavan7@gmail.com>
2020-05-21 17:14:15 +0530

    commit - v0.0.1 code added to test if async task out lives the activity lifecycle.

--- app/src/main/java/com/sharath/asynctask/MainActivity.java
+++ app/src/main/java/com/sharath/asynctask/MainActivity.java
@@ -2,13 +2,40 @@ package com.sharath.asynctask;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //new MyTask().execute();
+    }
+
+    public void onClick(View view) {
+        new MyTask().execute();
+    }
+
+    private class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            for (int i = 0; i<100; i++) {
+                Log.i(TAG, "Async task running");
+                try {
+                    Thread.sleep(500);
+                } catch (Exception e) {return null;}
+            }
+
+            return null;
+        }
     }
 }
